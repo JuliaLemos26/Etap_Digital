06d3e44ad6e85e865512ab38dfe8a7dafdd2bcac
julialemos26 <tgpsi23julialemos@etap.pt>
2025-10-26 18:33:41 +0000

    Menubarra

---
+++ etap_digital/etap_digital/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-wbrchie)vndn1af^4(xxe2h2xmn&#xm882x!o03kkozz*xu4+b"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
